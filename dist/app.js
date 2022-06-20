1dece9a3ff93b7d1c38edd47175ad6060401e346
cmjiwa <wangjinghuan@yahoo.com>
2022-06-20 19:16:22 +0300

    Getters and Setters

--- dist/app.js
+++ dist/app.js
@@ -19,9 +19,23 @@ class AccountingDepartment extends Department {
     constructor(id, reports) {
         super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("Report not found.");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("Please pass in a valid avlue");
+        }
+        this.addReport(value);
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     addEmployee(name) {
         if (name === "Max") {
@@ -34,9 +48,11 @@ class AccountingDepartment extends Department {
     }
 }
 const accounting = new AccountingDepartment("d22", []);
+accounting.addReport("Something went wrong....kkk");
+accounting.mostRecentReport = "hwllo rose";
+accounting.printReports();
+console.log(accounting.mostRecentReport);
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
-accounting.addReport("Something went wrong...");
 accounting.printEmployeeInformation();
-accounting.printReports();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,41 @@ class Department {
   }
 }
 class AccountingDepartment extends Department {
+  lastReport: string;
+
+  //create a getter
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("Report not found.");
+  }
+
+  //create a setter
+  set mostRecentReport(value: string) {
+    //if value ==='', then !value is true
+    if (!value) {
+      throw new Error("Please pass in a valid avlue");
+    }
+    this.addReport(value);
+  }
+
   //shorthand version of initializing
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+
+    //initialize lastReport if not show error
+    //it doesn't give the reports[0] to lastReport as its value
+    //therefore, this line of code doesn't conflict
+    //with Line43 (this.lastReport = text;)
+    // so, this line can be "this.lastReport = 'aaa'" for example.
+    this.lastReport = reports[0];
   }
   addReport(text: string) {
     this.reports.push(text); //reports is initialized in Line21
+
+    //this line is the codes which gives lastReport a value, not Line36
+    this.lastReport = text;
   }
   addEmployee(name: string) {
     if (name === "Max") {
@@ -37,9 +66,18 @@ class AccountingDepartment extends Department {
 }
 
 const accounting = new AccountingDepartment("d22", []);
+accounting.addReport("Something went wrong....kkk");
+
+//How to use a setter
+accounting.mostRecentReport = "hwllo rose";
+accounting.printReports();
+
+//How to use a getter
+//note: here not adding '()', but only "accounting.mostRecentReport"
+console.log(accounting.mostRecentReport);
+
 accounting.addEmployee("Max"); //Max will not be added into employees array
+
 accounting.addEmployee("Manu"); //Menu will be added into employees array
-accounting.addReport("Something went wrong...");
 
 accounting.printEmployeeInformation();
-accounting.printReports();
