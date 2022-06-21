d0fce437aa995eb7b3a2539b472b5b286373953b
cmjiwa <wangjinghuan@yahoo.com>
2022-06-21 14:50:17 +0300

    the firstt interface

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
+var user1;
+user1 = {
+    name: "rose",
+    age: 30,
+    greet: function (phrase) {
+        console.log(phrase + " " + this.name);
+    },
+};
+user1.greet("Hi there, ");
 //# sourceMappingURL=app.js.map
